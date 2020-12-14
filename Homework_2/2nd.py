39767026cd097b3bcffd8ba73066f9d66431c14e
lekhagreenwaves <75805305+lekhagreenwaves@users.noreply.github.com>
2020-12-15 00:26:57 +0300

    Delete 2nd.py

--- Homework_2/2nd.py
+++
@@ -1,5 +0,0 @@
-spisok = list(map(int, input("введите несколько чисел ").split(" ")))
-for el in range(1, len(spisok), 2):
-    spisok[el - 1], spisok[el] = spisok[el], spisok[el - 1]
-print(spisok)
-
