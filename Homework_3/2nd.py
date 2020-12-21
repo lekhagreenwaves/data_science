0fce3366d21b434aef6e4965ce3bb2315d44289f
lekhagreenwaves <lekha_greenwaves@mail.ru>
2020-12-21 20:58:45 +0300

    Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.

---
+++ Homework_3/2nd.py
@@ -0,0 +1,4 @@
+
+def people_data(name, surname, year, city):
+    return name, surname, year, city
+print("Имя, Фамилия, Год рождения, Город:", people_data(input("Введите имя: "), input("Введите фамилию: "), input("Введите год рождения: "), input("Введите город: ")))
